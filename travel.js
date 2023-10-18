ea835d0cda42f5b665eb6abe19f458585277ac23
Promtech <uwagboepromise5@gmail.com>
2023-10-18 12:41:43 +0100

    first commit

---
+++ travel.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu");
+const navMenu = document.getElementById("nav-menu")
+
+menu.addEventListener("click", ()=>{
+    console.log("It is clicked");
+    navMenu.classList.toggle("active");
+}) 
+
+
+let yes = "I did it"
+
+console.log(yes)
